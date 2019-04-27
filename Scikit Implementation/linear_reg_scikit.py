1003fb0b34ab255f79efd76be9ed920f128a2e13
Pratik Sawant <pratik_sawant@hotmail.com>
2019-04-27 17:05:45 +0530

    Linear Regression
    Scikit implementation and basic visualizations

--- Scikit Implementation/linear_reg_scikit.py
+++ Scikit Implementation/linear_reg_scikit.py
@@ -1,5 +1,6 @@
 import numpy as np 
-import pandas as pd 
+import pandas as pd
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("LinearRegression/Scikit Implementation/Salary_Data.csv")
 print(data)
@@ -7,8 +8,16 @@ print(data)
 X = data[['YearsExperience']]
 y = data['Salary']
 
+# Data Visualization
+plt.scatter(X, y)
+plt.xlabel('Years Experience')
+plt.ylabel('Salary')
+plt.title('Linear Regression')
+plt.show()
+
 print(X.shape, y.shape)
 
+# Model Building
 from sklearn.linear_model import LinearRegression
 model = LinearRegression()
 model.fit(X, y)
@@ -16,3 +25,20 @@ model.fit(X, y)
 testdata = np.array([[2.3]])
 prediction = model.predict(testdata)
 print("Prediction for 2.3 years of prediction: ", prediction[0])
+
+slope = model.coef_[0]
+y_intecept = model.intercept_
+print("\nEquation of line y = {}x + {}".format(slope, y_intecept))
+
+
+# Plotting the best fit line
+regressionline = []
+for x in data['YearsExperience']:
+    regressionline.append((slope*x) + y_intecept)
+
+plt.scatter(X, y)
+plt.plot(data['YearsExperience'], regressionline, c='r')
+plt.xlabel('Years Experience')
+plt.ylabel('Salary')
+plt.title('Linear Regression')
+plt.show()
